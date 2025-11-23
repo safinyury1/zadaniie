4b2f6923947b6521e3abefe0e0a94d70d8074a3a
safinyury1 <safin15092005@mail.ru>
2025-11-24 00:56:50 +0500

    Реализовано задание 3 миграции

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('middlename')->nullable();
+            $table->string('lastname');
+            $table->string('login');
+            $table->string('tel');
+            $table->string('role')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
