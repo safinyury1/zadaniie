f2050cc5e515df6b395444f7c83137de5ea1f8d1
safinyury1 <safin15092005@mail.ru>
2025-11-24 10:23:05 +0500

    zadanie 4 ready

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -9,4 +10,6 @@ class Report extends Model
 {
     use SoftDeletes;
     protected $guarded=[];
+
+
 }
